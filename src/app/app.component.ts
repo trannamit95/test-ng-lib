960994b97d1c5a173e4e564a705a44117c02081d
Nam Tran <nam.tran@bstarsolutions.com>
2019-06-04 23:32:36 +0700

    Update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-ng-lib';
+  avatar = 'http://www.myiconfinder.com/uploads/iconsets/256-256-6a9a5acd215316d5cd242192f19ba1ca-user.png';
+  logo = 'http://getbootstrap.com/docs/4.3/assets/brand/bootstrap-social.png';
+  headerMenus = [
+    {name: 'Home', url: '/'},
+    {name: 'About Us', url: '/about'},
+    {name: 'Products', url: '/product'},
+    {name: 'Contact', url: '/contact'}
+  ];
+  sideMenus = [
+    {name: 'Home', url: '/'},
+    {name: 'About Us', url: '/about'},
+    {name: 'Products', url: '/product'},
+    {name: 'Contact', url: '/contact'},
+    {name: 'Login', url: '/login'},
+    {name: 'Register', url: '/register'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExampleNg8LibModule } from 'example-ng8-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExampleNg8LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
